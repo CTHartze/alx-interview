c16fd17b73a208dd7cc761dfdc786bd4454b6ad9
Ismail Cayden <ismailmaho007@gmail.com>
2024-03-14 11:31:18 +0200

    Updated variable

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """Lockboxes"""
 
-def unlockBoxes(box_list):
+def unlockBoxes(boxes):
     """
     Function to determine if all the boxes can be opened.
     :param box_list: List of lists representing the locked boxes.
     :return: True if all boxes can be opened, else False.
     """
-    unlocked_boxes = [False] * len(box_list)  # Initialize boxes as locked
+    unlocked_boxes = [False] * len(boxes)  # Initialize boxes as locked
     unlocked_boxes[0] = True
 
-    for i in range(len(box_list)):
+    for i in range(len(boxes)):
         if unlocked_boxes[i]:
-            for key in box_list[i]:
-                if key < len(box_list) and not unlocked_boxes[key]:
+            for key in boxes[i]:
+                if key < len(boxes) and not unlocked_boxes[key]:
                     unlocked_boxes[key] = True
 
     return all(unlocked_boxes)
