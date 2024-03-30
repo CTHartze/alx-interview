c1d64e68236a2b0970663caea0e59d662330382e
CTHartze <ismailmaho007@gmail.com>
2024-03-30 02:22:26 -0700

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,48 +3,41 @@
 
 import sys
 
+line_count = 0
+total_file_size = 0
+status_code_counts = {'200': 0,
+                      '301': 0,
+                      '400': 0,
+                      '401': 0,
+                      '403': 0,
+                      '404': 0,
+                      '405': 0,
+                      '500': 0}
 
-def print_metrics(status_code_dict, total_size):
-    """
-    Function to print metrics
-    Args:
-        status_code_dict: dictionary of status codes
-        total_size: total size of the file
-    Returns:
-        None
-    """
-    print("Total File Size: {}".format(total_size))
-    for code, count in sorted(status_code_dict.items()):
-        if count != 0:
-            print("{}: {}".format(code, count))
-
-
-"""Initialization"""
-total_size = 0
-current_code = 0
-line_counter = 0
-status_code_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
-
-"""Processing input"""
 try:
-    for line in sys.stdin:  # loops through line
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]  # reverses list
-
-        if len(parsed_line) > 2:
-            line_counter += 1
-
-            if line_counter <= 10:
-                total_size += int(parsed_line[0])
-                current_code = parsed_line[1]
-
-                if current_code in status_code_dict.keys():
-                    status_code_dict[current_code] += 1
-
-            if line_counter == 10:
-                print_metrics(status_code_dict, total_size)
-                line_counter = 0
-
+    for line in sys.stdin:
+        line_parts = line.split(' ')
+        if len(line_parts) > 2:
+            status_code = line_parts[-2]
+            file_size = line_parts[-1]
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
+            total_file_size += int(file_size)
+            line_count += 1
+            if line_count == 10:
+                print('File Size: {:d}'.format(total_file_size))
+                sorted_keys = sorted(status_code_counts.keys())
+                for key in sorted_keys:
+                    value = status_code_counts[key]
+                    if value != 0:
+                        print('{}: {}'.format(key, value))
+                line_count = 0
+except Exception:
+    pass
 finally:
-    print_metrics(status_code_dict, total_size)
+    print('File Size: {:d}'.format(total_file_size))
+    sorted_keys = sorted(status_code_counts.keys())
+    for key in sorted_keys:
+        value = status_code_counts[key]
+        if value != 0:
+            print('{}: {}'.format(key, value))
