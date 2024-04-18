e70954077bb07942de427cc66dd53847d8efa096
CTHartze <ismailmaho007@gmail.com>
2024-04-18 13:10:04 -0700

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,21 +5,21 @@ const request = require('request');
 const filmNum = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-// Makes an API request to get film information
+// Makes API request to get film info
 request(filmURL + filmNum, async function (err, res, body) {
   if (err) return console.error(err);
 
-  // Parse the response body to get the list of character URLs
+  // Parse response body to get list of character URLs
   const charURLList = JSON.parse(body).characters;
 
-  // Iterare through the character URLs and fect character information
-  // Make a request to each character URL
+  // Iterate through character URLs and fetch info
+  // Make request to each character URL
   for (const charURL of charURLList) {
     await new Promise(function (resolve, reject) {
       request(charURL, function (err, res, body) {
         if (err) return console.error(err);
 
-        // Parse the charcter nformation and print the character's name Resolve the promise to indicate completion
+        // Parse character info and print character name
         console.log(JSON.parse(body).name);
         resolve();
       });
