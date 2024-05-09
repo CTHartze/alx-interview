35774d05699c123c4ef8377b63b776d8cac51389
CTHartze <ismailmaho007@gmail.com>
2024-05-09 15:01:06 -0700

    Island Perimeter Problem

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,11 +3,9 @@
 
 
 def island_perimeter(grid):
-    # A function that outputs the perimeter of an island described by grid
+    """A function that outputs the perimeter of an island described by grid"""
     perimeter = 0
     grid_length = len(grid)
-
-    # Iterate through each cell in the grid
     for row in range(grid_length):
         for column in range(len(grid[row])):
             if grid[row][column] == 1:
@@ -19,5 +17,4 @@ def island_perimeter(grid):
                     perimeter += 1
                 if row + 1 >= grid_length or grid[row + 1][column] == 0:
                     perimeter += 1
-
     return perimeter
