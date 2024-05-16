adeded7b8b728820bc3d1e78f4efcd839e6c4ed8
CTHartze <ismailmaho007@gmail.com>
2024-05-16 05:10:33 -0700

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,45 +1,32 @@
 #!/usr/bin/python3
-"""Prime Game problem"""
+""" Prime Game problem """
+
+
+def primeNumbers(n):
+    """ Determines prime numbers within parameters """
+    primeNos = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primeNos.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primeNos
 
 
 def isWinner(x, nums):
-    def sieve_of_eratosthenes(n):
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-        p = 2
-        while p * p <= n:
-            if primes[p]:
-                for i in range(p * p, n + 1, p):
-                    primes[i] = False
-            p += 1
-        return [i for i in range(n + 1) if primes[i]]
-
-    def play_game(n):
-        primes = sieve_of_eratosthenes(n)
-        maria_turn = True
-        while True:
-            if not primes:
-                return "Ben" if maria_turn else "Maria"
-            next_prime = primes[0]
-            if maria_turn:
-                maria_turn = False
-            else:
-                primes = [num for num in primes if num % next_prime != 0]
-                maria_turn = True
-
-    # Play each round and keep track of the winner
-    maria_wins = ben_wins = 0
-    for n in nums:
-        winner = play_game(n)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """ Determines the Prime Game winner """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
+    Maria = Ben = 0
+    for i in range(x):
+        primeNos = primeNumbers(nums[i])
+        if len(primeNos) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
