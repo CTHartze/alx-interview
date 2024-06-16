4a0b92a68655541e0e1b60f272f5b0824698b33c
CTHartze <ismailmaho007@gmail.com>
2024-06-16 10:11:07 -0700

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,27 +4,27 @@
 
 def validUTF8(data):
     """Checks if given data set represents a valid UTF-8 encoding"""
-    bytes_to_process = 0
-    
-    for byte in data:
-        if bytes_to_process == 0:
-            mask = 0b10000000
-            while mask & byte:
-                bytes_to_process += 1
+    num_bytes = 0
+
+    for num in data:
+
+        if num_bytes == 0:
+            mask = 1 << 7
+
+            while mask & num:
+                num_bytes += 1
                 mask >>= 1
 
-            if bytes_to_process == 0:
+            if num_bytes == 0:
                 continue
 
-            if bytes_to_process == 1 or bytes_to_process > 4 or byte >> 6 != 0b10:
+            if num_bytes == 1 or num_bytes > 4:
                 return False
+
         else:
-            if byte >> 6 != 0b10:
+            if not (num >> 6) == 0b10:
                 return False
 
-        bytes_to_process -= 1
+        num_bytes -= 1
 
-    if bytes_to_process != 0:
-        return False
-
-    return True
+    return num_bytes == 0
